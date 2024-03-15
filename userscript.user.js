97a8458e5d60fe28250b5e5330f193c061097cd6
Stefan K <skoshy@users.noreply.github.com>
2024-03-15 11:13:36 -0400

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -1,71 +1,133 @@
 // ==UserScript==
 // @name         YouTube Hide Chat by Default
 // @namespace    https://skoshy.com
-// @version      0.6.1
+// @version      0.7.1
 // @description  Hides chat on YouTube live streams by default
 // @author       Stefan K.
-// @match        https://*.youtube.com/*
+// @match        https://www.youtube.com/*
 // @updateURL    https://github.com/skoshy/YouTubeHideChatByDefaultUserscript/raw/master/userscript.user.js
-// @grant        none
+// @grant        GM.getValue
+// @grant        GM.setValue
 // @icon         https://youtube.com/favicon.ico
 // ==/UserScript==
 
-(function() {
-  "use strict";
-  const scriptId = "youtube-hide-chat-by-default";
+const scriptId = "youtube-hide-chat-by-default";
 
-  // configurable vars
-  // - if youtube decides to use a new button type, add it here
-  const buttonSelectors = ["button"];
-  // - for different languages for the HIDE CHAT text, add them here
-  const hideChatTexts = [
-    'HIDE CHAT', // english
-    'HIDE CHAT REPLAY', // english, chat replay
-    'OCULTAR CHAT', // spanish
-    'MASQUER LA CONVERSATION PAR CHAT', // french
-    'MASQUER LE CLAVARDAGE', // french canada
-    'NASCONDI CHAT', // italian
-    'OCULTAR CONVERSA', // portuguese
-    'চ্চাট লুকুৱাওক', // bengali
-    'च्याट लुकाउनुहोस्', // nepali
-    'चैट छिपाएं', // hindi
-    'チャットを非表示', // japanese
-    '隐藏聊天记录', // zh-Hans-CN
-    '隱藏即時通訊', // zh-Hant-TW and zh-Hant-HK
-  ];
+const UNIQUE_ID = (function getUniqueId() {
+  if (window.top !== window.self) {
+    const capturedUniqueId = new URL(window.location.href).searchParams.get(`${scriptId}-unique-id`);
 
-  const nodeIdString = `${scriptId}-id`;
+    if (!capturedUniqueId) {
+      throw new Error(`Unique ID was not properly passed to iFrame: ${window.location.href}`);
+    }
 
-  function log(...toLog) {
-    console.log(`[${scriptId}]:`, ...toLog);
+    log('Running in an iFrame, grabbed unique ID from URL', capturedUniqueId, window.location.href);
+
+    return capturedUniqueId;
   }
 
-  function getUrlSearchParams() {
-    return new URLSearchParams(document.location.search.substring(1));
+  return Math.floor(Math.random()*1000000);
+})();
+
+function log(...toLog) {
+  console.log(`[${scriptId}]:`, ...toLog);
+}
+
+const StorageClass = (scriptId, uniqueId, allowedKeys) => {
+  (async function updateSubStorageIds() {
+    const subStorageKey = `${scriptId}_base_subStorageIds`;
+    const subStorageIds = JSON.parse((await GM.getValue(subStorageKey)) || '{}');
+    console.log({subStorageIds});
+    await GM.setValue(subStorageKey, JSON.stringify({
+      ...subStorageIds,
+      [uniqueId]: {
+        dateCreated: Date.now(),
+      },
+    }));
+    const newSubStorageIds = (await GM.getValue(subStorageKey)) || {};
+    console.log('Set the value for subStorageIds', newSubStorageIds);
+  })();
+
+  const setVal = async (key, val) => {
+    if (!allowedKeys.includes(key)) {
+      throw new Error('Key not allowed');
+    }
+
+    await GM.setValue(`${scriptId}_${uniqueId}_${key}`, val);
+  }
+
+  const getVal = async (key) => {
+    if (!allowedKeys.includes(key)) {
+      throw new Error('Key not allowed');
+    }
+
+    return GM.getValue(`${scriptId}_${uniqueId}_${key}`);
+  };
+
+  return { setVal, getVal };
+};
+
+const { setVal, getVal } = StorageClass(scriptId, UNIQUE_ID, ['lastVidThatHidChat']);
+
+(function() {
+  "use strict";
+
+  // - if youtube decides to use a new button type, add it here
+  const buttonSelectors = ["button"];
+  const mutationObserverSelectors = [...buttonSelectors, 'iframe'];
+
+  function getRootUrlSearchParams() {
+    return new URL(window.location.href).searchParams;
+  }
+
+  function getCurrentVideoId() {
+    const v = getRootUrlSearchParams().get('v');
+
+    if (v) {
+      return v;
+    }
+
+    // if not the parent frame, then get it from the passed in iframe url params
+    return getRootUrlSearchParams().get(`${scriptId}-current-video-id`);
+  }
+
+  function findAncestorOfElement(el, findFunc) {
+    let currentEl = el;
+
+    while (currentEl?.parentElement) {
+      const result = findFunc(currentEl.parentElement);
+
+      if (result) {
+        return currentEl.parentElement;
+      }
+
+      currentEl = currentEl.parentElement;
+    }
+
+    return undefined;
   }
 
   function isHideChatButton(node) {
-    return hideChatTexts.includes(node.innerText.toUpperCase().trim());
-  }
+    const youtubeLiveChatAppAncestor = findAncestorOfElement(node, (parentEl) => {
+      return parentEl.tagName === 'YT-LIVE-CHAT-APP';
+    });
 
-  function getNodeId(node) {
-    let nodeId = node.getAttribute(nodeIdString);
+    if (!youtubeLiveChatAppAncestor) {
+      return false;
+    }
 
-    return nodeId;
-  }
-
-  function setAndGetNodeId(node, setIdTo) {
-    const originalNodeId = getNodeId(node);
-
-    const nodeId = setIdTo ? setIdTo : Math.random().toString();
-    node.setAttribute(nodeIdString, nodeId);
-
-    return { originalNodeId, nodeId };
+    return (node.getAttribute('aria-label') === 'Close');
   }
 
   function addedNodeHandler(node) {
+    if (!node.matches) return;
+
+    if (node.matches('iframe')) {
+      handleAddedIframe(node);
+      return;
+    }
+
     if (
-      !node.matches ||
       !buttonSelectors.some(b => node.matches(b))
     ) {
       return;
@@ -74,35 +136,37 @@
     if (isHideChatButton(node)) {
       log(`Found a hide-chat button`, node);
 
-      const { nodeId, originalNodeId } = setAndGetNodeId(node, getUrlSearchParams().get('v'));
+      const currentVid = getCurrentVideoId();
+      const lastVidThatHidChat = getVal('lastVidThatHidChat');
 
-      if (originalNodeId === nodeId) return; // we've already automatically triggered hide chat
+      if (lastVidThatHidChat === currentVid) {
+        log(`Already automatically triggered to hide chat for this video`, { lastVidThatHidChat, currentVid });
+        return;
+      }
 
-      log(`Hid the chat by default`, document.querySelector('yt-live-chat-message-input-renderer'));
+      log(`Attempting to hide the chat by default`, { lastVidThatHidChat, currentVid });
 
-      const tryToHideChatDetails = {
-        interval: null,
-        triedAttempts: 0,
-      };
+      setVal('lastVidThatHidChat', currentVid);
 
-      tryToHideChatDetails.interval = setInterval(() => {
-        tryToHideChatDetails.triedAttempts++;
-
-        if (document.querySelector('ytd-live-chat-frame iframe')?.offsetHeight ?? 0 > 0) {
-          log('Clicking again because live chat still there');
-          node.click();
-          tryToHideChatDetails.triedAttempts = 0;
-        }
-
-        if (tryToHideChatDetails.triedAttempts > 6) {
-          clearInterval(tryToHideChatDetails.interval);
-        }
-      }, 250);
-
-      return;
+      node.click();
     }
   }
 
+  function handleAddedIframe(node) {
+    if (node.getAttribute(`${scriptId}-modified-src`)) {
+      return;
+    }
+
+    const url = new URL(node.src);
+    url.searchParams.set(`${scriptId}-unique-id`, UNIQUE_ID);
+    url.searchParams.set(`${scriptId}-current-video-id`, getCurrentVideoId());
+    log('New iFrame URL', url.toString());
+
+    node.src = url.toString();
+    node.setAttribute(`${scriptId}-modified-src`, true);
+  }
+
+  /*
   const bodyObserver = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
       const newNodes = [];
@@ -112,7 +176,7 @@
 
         // it might be text node or comment node which don't have querySelectorAll
         if (addedNode.querySelectorAll) {
-          buttonSelectors.forEach(bs => {
+          mutationObserverSelectors.forEach(bs => {
             addedNode.querySelectorAll(bs).forEach((n) => {
               newNodes.push(n);
             });
@@ -123,17 +187,22 @@
       newNodes.forEach(n => addedNodeHandler(n));
     });
   });
+  */
 
   setInterval(() =>
-    Array.from(
-      document.querySelectorAll(buttonSelectors.join(', '))
-    ).forEach(n => addedNodeHandler(n))
-  , 3000);
+              Array.from(
+    document.querySelectorAll(mutationObserverSelectors.join(', '))
+  ).forEach(n => addedNodeHandler(n))
+              , 3000);
 
-  bodyObserver.observe(document.body, {
+  /*
+  bodyObserver.observe(document, {
     attributes: true,
     childList: true,
     subtree: true,
     characterData: true
   });
+  */
+
+  log('Initialized', UNIQUE_ID, window.location.href);
 })();
